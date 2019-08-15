cf3bbb6b7dcf9e28c18af5cb00642d67525c43e8
ShuheiIshihara <shuheinojobhunting@gmail.com>
2019-08-15 21:20:05 +0900

    # 機能追加 *コミット漏れを追加

---
+++ model/tUsebyDateInfo.go
@@ -0,0 +1,15 @@
+package model
+
+// 消費期限テーブル（トランザクション）
+type TUsebyDateInfo struct {
+	Id int `db:"ID" column:"id"`
+	// GOODS_ID    int64  `json:"goods_id"`
+	// USEBY_KBN   string `json:"useby_kbn"`
+	// LIMIT_DATE  string `json:"limit_date"`
+	// DELETE_FLAG string `json:"delete_flag"`
+	// DELETE_DATE string `json:"delete_date"`
+	// INSERT_USER string `json:"insert_user"`
+	// INSERT_DATE string `json:"insert_date"`
+	// UPDATE_USER string `json:"update_user"`
+	// UPDATE_DATE string `json:"update_date"`
+}
